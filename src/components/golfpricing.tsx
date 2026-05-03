a09d4fd761aacc06dc8e5971fd5257dba128014c
travisfriesen <github@travisfriesen.ca>
2026-05-02 22:28:24 -0400

    update golf pricing

--- src/components/golfpricing.tsx
+++ src/components/golfpricing.tsx
@@ -1,25 +1,30 @@
-import { Spacer } from "./spacer"
+import { Spacer } from "./spacer";
 
 export const GolfPricing = () => {
-    return (
-        <div className="pl-5 pr-5">
-        <div className=" rounded-2xl w-fit bg-r-black h-180 p-8 tracking-wide flex flex-col space-between">
-            <h1 className="text-4xl text-center pb-3 text-r-text">Driving Range Pricing</h1>
-            <div className="grid grid-cols-2">
-                <div className="text-r-text">
-            Small (26 balls): <br/>
-            Medium (52 balls): <br/>
-            Large (104 balls): <br/>
-                </div>
-                <div className="text-right text-r-text">
-                    $6 <br/>
-                    $12 <br/>
-                    $22 <br/>
-                </div>
+  return (
+    <div className="pl-5 pr-5">
+      <div className=" rounded-2xl w-fit bg-r-black h-180 p-8 tracking-wide flex flex-col space-between">
+        <h1 className="text-4xl text-center pb-3 text-r-text">
+          Driving Range Pricing
+        </h1>
+        <div className="grid grid-cols-2">
+          <div className="text-r-text">
+            Small (26 balls): <br />
+            Medium (52 balls): <br />
+            Large (104 balls): <br />
+            Saturday Special: 3 Large's: <br />
+          </div>
+          <div className="text-right text-r-text">
+            $7 <br />
+            $14 <br />
+            $25 <br />
+            $30 <br />
+          </div>
         </div>
-        <Spacer/>
+        <Spacer />
         <p className="italic">Buy 10 buckets and get your 11th bucket free!</p>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
